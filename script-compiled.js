5a1696c6f27ba9ca23a2b35c0f7ef14865afc9ee
Tina Giska <tina.tylicka@gmail.com>
2018-05-09 12:19:34 +0200

    Update files

--- script-compiled.js
+++ script-compiled.js
@@ -33,18 +33,48 @@ var Stopwatch = function () {
         value: function format(times) {
             return pad0(times.minutes) + ':' + pad0(times.seconds) + ':' + pad0(Math.floor(times.miliseconds));
         }
+    }, {
+        key: 'start',
+        value: function start() {
+            var _this = this;
+
+            if (!this.running) {
+                this.running = true;
+                this.watch = setInterval(function () {
+                    return _this.step();
+                }, 10);
+            }
+        }
+    }, {
+        key: 'step',
+        value: function step() {
+            if (!this.running) return;
+            this.calculate();
+            this.print();
+        }
+    }, {
+        key: 'calculate',
+        value: function calculate() {
+            this.times.miliseconds += 1;
+            if (this.times.miliseconds >= 100) {
+                this.times.seconds += 1;
+                this.times.miliseconds = 0;
+            }
+            if (this.times.seconds >= 60) {
+                this.times.minutes += 1;
+                this.times.seconds = 0;
+            }
+        }
+    }, {
+        key: 'stop',
+        value: function stop() {
+            this.running = false;
+            clearInterval(this.watch);
+        }
     }]);
 
     return Stopwatch;
 }();
-
-function pad0(value) {
-    var result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
-}
 
 var stopwatch = new Stopwatch(document.querySelector('.stopwatch'));
 
@@ -57,3 +87,11 @@ var stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', function () {
     return stopwatch.stop();
 });
+
+function pad0(value) {
+    var result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
